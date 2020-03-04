48cf5b5d092f642b3836f1a64ba2534d2f173358
scheitanist <iaslezkin@yandex.ru>
2020-03-04 16:56:41 +0700

    footer, card - end

--- app/js/script.js
+++ app/js/script.js
@@ -4,6 +4,8 @@ $(document).ready(function(){
 		margin: 10,
 		lazyLoad: true,
 		loop: true,
+		autoplay: true,
+		autoplayTimeout: 3000,
     	navText : ['<i class="fas fa-chevron-left"></i>','<i class="fas fa-chevron-right"></i>'],
     	nav: true,
 		responsive: {
@@ -27,4 +29,46 @@ $(document).ready(function(){
 			}
 		}
 	});
+
+	
+
+	
+	
+
+
 });
+
+
+
+App = {
+
+	// Header
+	headerTopMenuToogle: function()
+	{
+		// document.addEventListener("scroll", function(){
+		// 	if(window.pageYOffset >= 300)
+		// 	{
+		// 		if(window.pageYOffset >= 00)
+		// 		{
+		// 			$($("header .row")[0]).slideUp(50)
+		// 		}
+		// 	}
+		// 	else
+		// 	{
+		// 		$($("header .row")[0]).slideDown(50)
+		// 	}
+		// })
+	},
+
+	// initialization
+	init: function()
+	{
+		this.headerTopMenuToogle();
+	}
+
+}
+
+
+document.addEventListener("DOMContentLoaded", function(){
+	App.init();
+})
